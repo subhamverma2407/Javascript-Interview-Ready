dd303d624f3f3e56af8450b09415d7bbf8e4575d
Subhamverma2407 <subhamverma2407@gmail.com>
2024-01-31 14:31:39 +0530

    Fixed bind polyfill to use symbol as key

--- Polyfills/bind.js
+++ Polyfills/bind.js
@@ -6,11 +6,11 @@ const print = function (greetings, message) {
   console.log(`${greetings}! ${this.title} ${message}`);
 };
 Function.prototype.myBind = function (obj, ...args) {
-  const incSymbol = Symbol("functionToCall");
-  obj.fncSymbol = this;
+  const fncSymbol = Symbol("functionToCall");
+  obj[fncSymbol] = this;
   return function (...args1) {
-    obj.fncSymbol(...args, ...args1);
-    delete obj.fncSymbol;
+    obj[fncSymbol](...args, ...args1);
+    delete obj[fncSymbol];
   };
 };
 const printData = print.myBind(data, "Hi", "is a great language");
