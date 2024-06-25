abfd327df33eb79966b6df661eefd16fd5bcc981
Subhamverma2407 <subhamverma2407@gmail.com>
2024-06-26 01:43:06 +0530

    Modified push, pop, shift,unshift

--- ArrayMethods/shift.js
+++ ArrayMethods/shift.js
@@ -1,7 +1,10 @@
 // It removes element form start of the array.
 // Modifies original array
 // it returns removed element
+// if element is empty then undefined is returned
 
-const arr = [1, 2, 3, 4, 5, 6];
+const arr = [1, , 2, 3, 4, 5, 6];
 const res = arr.shift();
+const res1 = arr.shift();
 console.log(res); // 1
+console.log(res1); // undefined
--- ArrayMethods/unshift.js
+++ ArrayMethods/unshift.js
@@ -1,6 +1,6 @@
 // this method is used to add element/elements at the start of an array.
 // This method changes the original array and its length.
-// It returns the length of modified array
+// It returns the length of modified array.
 
 const arr = [1, 2, 3, 4, 5];
 const res = arr.unshift("abc", "def");
