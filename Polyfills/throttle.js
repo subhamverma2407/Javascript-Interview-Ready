574716bba225d1afce845dfb8128df0ce7a95f3c
Subhamverma2407 <subhamverma2407@gmail.com>
2024-07-02 18:25:33 +0530

    added description in throttle func

--- Polyfills/throttle.js
+++ Polyfills/throttle.js
@@ -1,3 +1,6 @@
+// In throttle, once we invoke the throttled function then it will first execute the main function.
+// then it will wait for the delay and triggers the main function after every delay.
+
 const myThrottle = function (func, delay) {
   let isThrottled = false;
   return function () {
