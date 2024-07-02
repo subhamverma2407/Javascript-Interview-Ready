54d93099dc4d3f702177338b92c29b9e3f775986
Subhamverma2407 <subhamverma2407@gmail.com>
2024-07-02 21:19:08 +0530

    Updated bind polyfill

--- Polyfills/bind.js
+++ Polyfills/bind.js
@@ -5,23 +5,34 @@ const data = {
 const print = function (greetings, message) {
   console.log(`${greetings}! ${this.title} ${message}`);
 };
-Function.prototype.myBind = function (obj, ...args) {
-  const fncSymbol = Symbol("functionToCall");
-  obj[fncSymbol] = this;
-  return function (...args1) {
-    obj[fncSymbol](...args, ...args1);
-    delete obj[fncSymbol];
+
+Function.prototype.myBind = function (context = {}, ...args) {
+  context.fn = this;
+  return function () {
+    context.fn(...args);
   };
 };
 
-Function.prototype.newBind = function (...args) {
-  const obj = this;
-  const rest = args.slice(1);
-  return function (...args1) {
-    obj.apply(args[0], rest.concat(args1));
-  };
-};
-const printData = print.myBind(data, "Hi", "is a great language");
-const printData1 = print.newBind(data, "Hello", "is the best language");
-printData1(); // Hi! Javascript is a great language
-printData1(); // Hello!! Javascript is the best language
+// Function.prototype.myBind = function (obj, ...args) {
+//   const fncSymbol = Symbol("functionToCall");
+//   obj[fncSymbol] = this;
+//   return function (...args1) {
+//     obj[fncSymbol](...args, ...args1);
+//     delete obj[fncSymbol];
+//   };
+// };
+
+// Function.prototype.newBind = function (...args) {
+//   const obj = this;
+//   const rest = args.slice(1);
+//   return function (...args1) {
+//     obj.apply(args[0], rest.concat(args1));
+//   };
+// };
+// const printData = print.myBind(data, "Hi", "is a great language");
+// const printData1 = print.newBind(data, "Hello", "is the best language");
+// printData1(); // Hi! Javascript is a great language
+// printData1(); // Hello!! Javascript is the best language
+
+const printData2 = print.myBind(data, "Hi", "is a great language");
+printData2(); // Hi! Javascript is a great language
