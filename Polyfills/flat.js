b94e58623b17575d56c07afdec3605731a1d29e5
Subhamverma2407 <subhamverma2407@gmail.com>
2024-07-02 20:14:50 +0530

    updated flat polyfill

--- Polyfills/flat.js
+++ Polyfills/flat.js
@@ -12,3 +12,20 @@ const flat = (arr) => {
 };
 
 console.log(flat(arr));
+
+const newArr = [1, 2, [3, 4, 5, [6, 7, 8, [9, 10]]]];
+
+Array.prototype.myFlat = function () {
+  let output = [];
+  function flatten(arr) {
+    arr.forEach((val) => {
+      if (Array.isArray(val)) {
+        flatten(val);
+      } else output.push(val);
+    });
+    return output;
+  }
+  return flatten(this);
+};
+
+console.log(newArr.myFlat());
