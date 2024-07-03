aefe3572e5a24cdde4ee6f804b56e3720c656216
Subhamverma2407 <subhamverma2407@gmail.com>
2024-07-03 14:31:32 +0530

    sort array of objects based on key

---
+++ coding-questions/objects/1. sort_array_of_object.js
@@ -0,0 +1,42 @@
+const employees = [
+  {
+    name: "Alice",
+    department: "Engineering",
+    salary: 75000,
+  },
+  {
+    name: "Charlie",
+    department: "HR",
+    salary: 55000,
+  },
+  {
+    name: "Bob",
+    department: "Marketing",
+    salary: 60000,
+  },
+  {
+    name: "Eve",
+    department: "Finance",
+    salary: 70000,
+  },
+  {
+    name: "David",
+    department: "Engineering",
+    salary: 80000,
+  },
+];
+
+// Sort based on name;
+
+const res = employees.slice().sort((a, b) => {
+  if (a.department < b.department) {
+    return -1;
+  }
+  if (a.department > b.department) {
+    return 1;
+  } else return 0;
+});
+console.log(res);
+
+const res1 = employees.slice().sort((a, b) => a.name.localeCompare(b.name)); // more elegant way
+console.log(res1);
