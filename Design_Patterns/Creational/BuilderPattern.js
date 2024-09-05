dc2b6632a9319ddae07ddac31996a7dc5ebed214
Subhamverma2407 <subhamverma2407@gmail.com>
2024-09-06 01:06:51 +0530

    Added design patterns

--- Design_Patterns/Creational/BuilderPattern.js
+++ Design_Patterns/Creational/BuilderPattern.js
@@ -32,3 +32,37 @@ const person1 = new PersonBuilder("subham", "M")
   .setWeight(54)
   .build();
 console.log(person1);
+
+/// Another Approach
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class UserBuilder {
+  constructor(name) {
+    this.user = new User(name);
+  }
+
+  setAge(age) {
+    this.user.age = age;
+    return this;
+  }
+
+  setPhone(phone) {
+    this.user.phone = phone;
+    return this;
+  }
+
+  setAddress(address) {
+    this.user.address = address;
+    return this;
+  }
+  build() {
+    return this.user;
+  }
+}
+
+const user = new UserBuilder("subham").setAge(27).setPhone(827827382).build();
+console.log(user);
---
+++ JS tuts/index.js
@@ -0,0 +1,67 @@
+// // let a = 10;
+// // console.log(typeof a);
+
+// // var , let and const
+
+// // ES6 (let const)
+
+// // global scope
+// // functional scope
+// // block scope
+// // module scope
+// var b = 20;
+// let a = 10;
+// if (a) {
+//   var b = 30;
+// }
+
+// console.log(b);
+
+// // hoisting
+
+// function test() {
+//   console.log(30);
+// }
+// var b = 20;
+
+// var b = undefined;
+// function test() {
+//   console.log(30);
+//   function test1() {
+//     console.log("test1");
+//     var c = 10;
+//   }
+// }
+// b = 20;
+
+// var moto = undefined;
+// function a() {}
+// moto = 10;
+
+// console.log(foo); // undefined
+
+// var foo = "bar";
+
+// console.log(foo); // "bar"
+
+// var foo = undefined;
+// console.log(foo);
+
+// console.log(a);
+// var a = 10;
+
+// scoping
+// let a = 10;
+
+// const a = [1, 2, 3]; //&201
+// a[0] = 20;
+// console.log(a);
+
+let num = 10;
+let dec = 10.4;
+// console.log(typeof dec);
+let bool = true;
+let str = "moto";
+console.log(str);
+
+let a;
